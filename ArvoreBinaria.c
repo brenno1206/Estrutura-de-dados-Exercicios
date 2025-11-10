949cb46cb13da94516330f601f40d0d8dae9c160
Brenno <151473774+brenno1206@users.noreply.github.com>
2025-11-10 08:51:05 -0300

    Refactor binary tree functions and update values

--- ArvoreBinaria.c
+++ ArvoreBinaria.c
@@ -51,11 +51,36 @@ int totalNosArvBin(ArvBin *raiz) {
 }
 
 int alturaArvBin(ArvBin *raiz) {
-    if (raiz == NULL || *raiz == NULL) return -1;
-    int esq = alturaArvBin(&((*raiz)->esq));
-    int dir = alturaArvBin(&((*raiz)->dir));
-    if (esq > dir) return esq + 1;
-    return dir + 1;
+	if (raiz == NULL || *raiz == NULL) return -1;
+	int esq = alturaArvBin(&((*raiz)->esq));
+	int dir = alturaArvBin(&((*raiz)->dir));
+	if (esq > dir) return esq + 1;
+	return dir + 1;
+}
+
+void inserirElemento(ArvBin *raiz, int valor) {
+	if (raiz == NULL) return;
+	NO* no = (NO*) malloc(sizeof(NO));
+	no->info = valor;
+	no->dir = NULL;
+	no->esq = NULL;
+	if(*raiz == NULL) *raiz = no;
+	else {
+		if((*raiz)->info == valor) return;
+		if(valor < (*raiz)->info) return inserirElemento(&((*raiz)->esq), valor);
+		else return inserirElemento(&((*raiz)->dir), valor);
+	}
+}
+
+int buscarElemento(ArvBin *raiz, int valor) {
+	if(raiz == NULL || *raiz == NULL) return 0;
+	if (((*raiz)->info) == valor) return 1;
+
+	if (((*raiz)->info) > valor)
+		return buscarElemento(&((*raiz)->esq), valor);
+	else
+		return buscarElemento(&((*raiz)->dir), valor);
+
 }
 
 int main()
@@ -67,37 +92,45 @@ int main()
 	NO a,b, c, d, e, f, g, h, i;
 	NO *converte = &a;
 
-	a.info = 15;
+	a.info = 80;
 	a.esq = &b;
 	a.dir = &c;
-	b.info = 20;
+	b.info = 60;
 	b.esq = &d;
 	b.dir = &e;
-	c.info = 7;
+	c.info = 90;
 	c.esq = &f;
 	c.dir = &g;
-	d.info = 2;
+	d.info = 50;
 	d.esq = NULL;
 	d.dir = NULL;
-	e.info = 32;
+	e.info = 70;
 	e.esq = &h;
 	e.dir = NULL;
-	h.info = 5;
+	h.info = 65;
 	h.esq = NULL;
 	h.dir = NULL;
-	f.info = 3;
+	f.info = 85;
 	f.esq = NULL;
 	f.dir = NULL;
-	g.info = 10;
+	g.info = 110;
 	g.esq = &i;
 	g.dir = NULL;
-	i.info = 1;
+	i.info = 100;
 	i.esq = NULL;
 	i.dir = NULL;
 
 	raiz = &converte;
 
-	printf(" preOrdem: ");
+	inserirElemento(raiz, 19);
+
+
+	printf("\n 110: %d", buscarElemento(raiz, 110));
+	printf("\n 50: %d", buscarElemento(raiz, 50));
+	printf("\n 2: %d", buscarElemento(raiz, 2));
+	printf("\n 4: %d", buscarElemento(raiz, 4));
+
+	printf("\n preOrdem: ");
 	preOrdem(raiz);
 	printf("\n emOrdem:  ");
 	emOrdem(raiz);
